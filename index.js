617e0cc266569a379be58fb05684e1ea569c3081
mark <mark16calle@gmail.com>
2022-09-09 12:46:43 -0500

    Nuevo login y estilos css

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express=require('express')
-const app=express()
-app.use(express.static(__dirname + '/public'))
-app.get('/hola',(req,res)=>{
-    res.send("Ruta")
+const express = require("express");
+const app = express();
+app.use(express.static(__dirname + "/public"));
+app.get("/hola", (req, res) => {
+  res.send("Ruta");
+});
+app.post('/login',(req,res)=>{
+    res.send("Ruta de login")
 })
-
-app.listen(3000,()=>{console.log("listening on port 3000")})
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
