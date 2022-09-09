a51764e3770be015ed8f9a23dc99ed9a47eaf0d1
mark <mark16calle@gmail.com>
2022-09-09 00:32:57 -0500

    Ruta añadida

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express=require('express')
 const app=express()
 app.use(express.static(__dirname + '/public'))
+app.get('/hola',(req,res)=>{
+    res.send("Ruta")
+})
 
 app.listen(3000,()=>{console.log("listening on port 3000")})
